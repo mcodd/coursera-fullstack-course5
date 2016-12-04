cbf1f8c9a9a67f5391e944c52e387a5a884d5ae7
Matt Coddington <coddington@gmail.com>
2016-12-04 15:18:03 -0500

    module2 solution

---
+++ module2-solution/app.js
@@ -0,0 +1,56 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+  toBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+
+  // This is the function called when the "Bought" button is clicked
+  toBuy.buyItem = function (itemIndex) {
+    ShoppingListCheckOffService.buyItem(itemIndex);
+  }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var alreadyBought = this;
+  alreadyBought.items = ShoppingListCheckOffService.getItemsAlreadyBought();
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of items still to buy
+  var itemsToBuy = [
+    { name: "cookies", quantity: 10 },
+    { name: "bottles of milk", quantity: 2 },
+    { name: "cakes", quantity: 3 }
+  ];
+  // List of items we've already bought
+  var itemsAlreadyBought = [];
+
+  // the buyItem function removes the item with index itemIndex from
+  // itemsToBuy and adds it to itemsAlreadyBought
+  service.buyItem = function (itemIndex) {
+    var itemBought = itemsToBuy[itemIndex];
+    itemsToBuy.splice(itemIndex, 1);
+    itemsAlreadyBought.push(itemBought);
+  };
+
+  service.getItemsToBuy = function () {
+    return itemsToBuy;
+  };
+
+  service.getItemsAlreadyBought = function () {
+    return itemsAlreadyBought;
+  };
+
+}
+
+})();
